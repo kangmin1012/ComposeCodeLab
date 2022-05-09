b0c0c3058ae8b26f4b0f564cd45d0db9da544b00
kangmin1012 <kangmin1012@naver.com>
2022-05-09 17:51:41 +0900

    ComposeLayout - LazyColumn

--- ComposeLayout/app/src/main/java/com/example/composelayout/MainActivity.kt
+++ ComposeLayout/app/src/main/java/com/example/composelayout/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -15,19 +17,24 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composelayout.ui.theme.ComposeLayoutTheme
+import com.skydoves.landscapist.glide.GlideImage
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutTheme {
+                LazyList()
             }
         }
     }
@@ -104,6 +111,75 @@ fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+/** RecyclerView를 Compose에서 구현 */
+@Composable
+fun LazyList() {
+    val listSize = 100
+    // 현재 Scroll 상태값을 저장하는 객체
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column {
+            Row {
+                Button(
+                    onClick = {
+                        /** 스크롤을 최상단으로 이동 */
+                        coroutineScope.launch {
+                            scrollState.animateScrollToItem(0)
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(text = "Scroll to the top")
+                }
+
+                Button(
+                    onClick = {
+                        /** 스크롤을 최하단으로 이동 */
+                        coroutineScope.launch {
+                            scrollState.animateScrollToItem(listSize - 1)
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(text = "Scroll to the end")
+                }
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                state = scrollState
+            ) {
+                items(listSize) { item ->
+                    ImageListItem(index = item)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        GlideImage(
+            imageModel = "https://developer.android.com/images/brand/Android_Robot.png",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(50.dp)
+        )
+
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
