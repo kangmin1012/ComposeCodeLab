d7e55e2553e2610923f181cefb12ff8d754de608
kangmin1012 <kangmin1012@naver.com>
2022-05-09 16:25:54 +0900

    ComposeLayout - Modifier 설정

--- ComposeLayout/app/src/main/java/com/example/composelayout/MainActivity.kt
+++ ComposeLayout/app/src/main/java/com/example/composelayout/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.composelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelayout.ui.theme.ComposeLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp) // 상위 레이아웃과의 패딩값
+            .clip(RoundedCornerShape(4.dp)) // 모서리 둥글게
+            .background(MaterialTheme.colors.surface)
+            .clickable {
+                // todo :  Ignoring onClick
+            }
+            .padding(16.dp) // Row 안의 패딩값
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // 프로필 이미지가 보여질 자리
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
-fun DefaultPreview() {
+fun PhotographerPreview() {
     ComposeLayoutTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
