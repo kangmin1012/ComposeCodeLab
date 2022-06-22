6d8d46ae37ebdbf955d787e656b759d52c5ba77e
kangmin1012 <kangmin1012@naver.com>
2022-06-22 11:31:09 +0900

    CustomLayout - Staggered

---
+++ ComposeLayoutLab/app/src/main/java/com/example/composelayoutlab/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.example.composelayoutlab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.composelayoutlab.ui.theme.ComposeLayoutLabTheme
+import kotlin.math.max
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLayoutLabTheme {
+                BodyContent()
+            }
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier, rows = 5) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Layout의 하위 요소는 한 번만 측정 가능
+
+        // 각 행의 너비 추적
+        val rowWidths = IntArray(rows) { 0 }
+
+        // 각 행의 최대 높이 추적
+        val rowHeights = IntArray(rows) { 0 }
+
+        // 측정된 자식 뷰 리스트
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            // 자식 뷰 측정
+            val placeable = measurable.measure(constraints)
+
+            // 각 행에 대한 너비와 최대 높이 추적
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        /** 화면에 배치하기 전 Grid의 크기 계산 */
+        // Grid의 너비 = 가장 넓은 너비를 가진 행
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        // Grid의 높이 = 모든 행의 높이
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        /** 부모 레이아웃 크기 설정 */
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+
+    }
+}
+
+/** 실습용 Chip */
+@Composable
+fun Chip(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(Dp.Hairline, Color.Black),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ActivityPreview() {
+    ComposeLayoutLabTheme {
+        BodyContent()
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    ComposeLayoutLabTheme {
+        Chip(text = "Hi there")
+    }
+}
